036fab08dce0e9b19ea0cc6f3b0b68b04ebbfcd3
Kai <kai.hafner@outlook.com>
2025-10-20 14:11:27 +0100

    Initial commit

---
+++ Source/PSGAM_DungeonGen/PSGAM_DungeonGen.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PSGAM_DungeonGen.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PSGAM_DungeonGen, "PSGAM_DungeonGen" );
